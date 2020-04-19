e45a978dfc6e792cafc5666caea73ff74645e799
edidi27 <erindidier@gmail.com>
2020-04-18 21:53:39 -0400

    adjusted CSS + added js file and gif folder

---
+++ js.js
@@ -0,0 +1,10 @@
+
+
+//BURRITO
+/*take burrito user input and write over speech bubble */
+
+//HOW DID I DIE
+/*randomize output of how person died and write text + gif to page */
+
+//FROGS
+/*write frog img to page every time user presses give jeff a frog button*/
