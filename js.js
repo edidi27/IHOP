2a29563ee65e235111afc82c7b7c023f9f386b52
edidi27 <erindidier@gmail.com>
2020-05-03 00:43:49 -0400

    Burrito CSS + Added Janet's Void and link + animated void message + added how your friend died button

--- js.js
+++ js.js
@@ -12,16 +12,16 @@ var burrito_input = document.getElementById("speech");
 
 
 //HOW YOU DIED
-//randomizes selection of Cause of Death and Reaction Gif after button click & writes it to HTML
+/*randomizes selection of Cause of Death and Reaction Gif after button click & writes it to HTML*/
 
 
 let COD = [
-    /*0*/ "You bet your friend you could  knock down a tree by kicking it over. You disrupted a bee hive and got stung to death.",
+    /*0*/ "You bet your friend you could  knock down a tree with a karate kick. You disrupted a bee hive and got stung to your death.",
     /*1*/ "You fought an old granny for the last package of toilet paper at the supermarket and caught your foot on the display. As you tried to yank it out, a porcelain toilet seat fell from the top shelf and knocked you out.",
     /*2*/ "You were walking with your mom and got angry at her, so you stepped on a crack in the sidewalk. Her back broke and she fell onto you, toppling you to the ground, where your head landed on a sharp rock.",
     /*3*/ "You thought the coronavirus pandemic was a hoax, so you organized a coronavirus party, livestreamed the whole thing, caught the disease, and perished.",
-    /*4*/ "You pushed a kid off of a swing because all of the swings were full. At the highest pont of your swing, the chain broke and you fell violently to the ground.",
-    /*5*/ "You tried to run up the down escalator, tripped, and toppled down to your death.",
+    /*4*/ "You pushed a kid off of a swing because all of the swings were full. At the highest point of your swing, the chain broke. You yelled, \"I can't be tamed!!!\" as you fell to your demise.",
+    /*5*/ "You tried to run up the down escalator, tripped, and toppled to your death.",
     /*6*/ "You jumped into a tiger exhibit at the zoo to pet it and became a tiger meal.",
     /*7*/ "You tried the cinnamon challenge and choked on the cinnamon. At least you went viral.",
     /*8*/ "You used a ouiji board to summon the dark spirits. When you hadn't left your room before dinner the next day, your roommate checked up on you and found you breathless lying on the floor, one hand on the board and the other on your heart.",
@@ -42,20 +42,38 @@ let gif = [
     /*10*/'<img src="img/RIPgifs/giphy(10).gif" alt="Gif reaction">',
     /*11*/'<img src="img/RIPgifs/giphy(11).gif" alt="Gif reaction">',
     /*12*/'<img src="img/RIPgifs/giphy(12).gif" alt="Gif reaction">',
-    /*13*/'<img src="img/RIPgifs/giphy(13).gif" alt="Gif reaction">',
-    /*14*/'<img src="img/RIPgifs/giphy(14).gif" alt="Gif reaction">'
+    /*13*/'<img src="img/RIPgifs/giphy(13).gif" alt="Gif reaction">'
     ];
 
 
-var death_button = document.getElementById("death_btn");
+var death_button = document.getElementById("death_btn_1");
+
+//death_button.addEventListener("click", ()=>{
+//    let death_index = Math.floor(Math.random()*COD.length);
+//    let gif_index = Math.floor(Math.random()*gif.length);
+//    document.getElementById('die_html_output').innerHTML = `<p> ${COD[death_index]} </p> ${gif[gif_index]}  <button type="button" class="death_btns id="death_btn_2">Discover <br> how your <br> friend died </button>`;
+//    document.getElementById('death_btn_1').style.display = 'none';
+//    });
 
 death_button.addEventListener("click", ()=>{
     let death_index = Math.floor(Math.random()*COD.length);
     let gif_index = Math.floor(Math.random()*gif.length);
-    document.getElementById('die_html_output').innerHTML = `<p> ${COD[death_index]} </p> ${gif[gif_index]}`});
+    document.getElementById('die_html_output').innerHTML = `<p> ${COD[death_index]} </p> ${gif[gif_index]}`;
+    document.getElementById('death_btn_1').style.display = 'none';
+    death_button= document.getElementById('new_die_button')
+    death_button.innerHTML = `<button type="button" class="death_btns id="death_btn_2">Discover <br> how your <br> friend died </button>`;
+});
+
+
+
+//function dynamicEvent(){
+//    console.log("Event happened");
+//}
+//
+//var element = document.createElement('p');
+//element.innerHTML= JS TEXT IS HERRE;
+//element.onclick = dynamicEvent;
+
 
 
 //JANET'S VOID
-
-//FROGS
-/*write frog img to page every time user presses give jeff a frog button*/
